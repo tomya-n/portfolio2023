e9589f16583574916ff868e047c4eec36d3ddd7f
tom <tomcan1126@gmail.com>
2023-06-25 20:41:40 +0900

    styled-reset インストール

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,23 @@ import React from 'react';
 import Header from './component/header/Header';
 import Main from './component/main/Main'
 import Footer from './component/footer/Footer'
-import './App.css';
+// import './App.css';
+import styled from 'styled-components';
+
+
+const AppInner = styled.div`
+  display: flex;
+  flex-flow: column;
+  min-height: 100vh;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+      <AppInner>
+        <Header />
+          <Main />
+        <Footer />
+      </AppInner>
   );
 }
 
